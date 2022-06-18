f52d4ce03790e1822bcc8aef460f9046f18dc0c3
namhoon <mook8848@naver.com>
2022-06-18 12:58:45 +0900

    Project 생성
    IntelliJ의 spring boot template을 이용한 프로젝트 생성

---
+++ src/main/java/com/triple/mileage/Triple2002Application.java
@@ -0,0 +1,13 @@
+package com.triple.mileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Triple2002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Triple2002Application.class, args);
+    }
+
+}
